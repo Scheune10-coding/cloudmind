297d96090d123b64c8420a9c92a3e740d8289421
Scheune10-coding <scheunemannleon34@gmail.com>
2026-03-23 12:04:45 +0100

    Add status codes

--- src/server/response.py
+++ src/server/response.py
@@ -2,9 +2,17 @@ import json
 
 STATUS = {
   200: 'OK',
+  201: 'Created',
+  202: 'Accepted',
   400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
   404: 'Not Found',
-  500: 'Internal Server Error'
+  405: 'Method Not Allowed',
+  408: 'Request Timeout',
+  500: 'Internal Server Error',
+  501: 'Not Implemented',
+  502: 'Bad Gateway'
 }
 
 class Response:
